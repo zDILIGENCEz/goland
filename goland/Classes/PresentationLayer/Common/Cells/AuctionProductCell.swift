c120d4e6cefb85be854d9a207e2f6b861cb3b2ad
Nikita Kusyumov <n.kusyumov@mail.ru>
2021-03-05 01:03:18 +0300

    End draw layer. Start create model.

--- goland/Classes/PresentationLayer/Common/Cells/AuctionProductCell.swift
+++ goland/Classes/PresentationLayer/Common/Cells/AuctionProductCell.swift
@@ -17,6 +17,12 @@ class AuctionProductCell: UICollectionViewCell {
     private struct Appearance: Grid {
         let endViewCornerRadius: CGFloat = 11
         let endViewBorderWidth: CGFloat = 1
+        let endViewOffsets: CGFloat = 6
+        let endViewHeight: CGFloat = 22
+        let auctionImageViewOffset: CGFloat = 2
+        let auctionImageViewSize: CGFloat = 18
+        let productImageViewWidth: CGFloat = 163
+        let productImageViewHeight: CGFloat = 96
     }
     private let appearance = Appearance()
 
@@ -45,6 +51,12 @@ class AuctionProductCell: UICollectionViewCell {
         let label = UILabel()
         label.bonMotStyle = .bold10(.black)
         return label
+    }()
+    
+    lazy var closeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
     }()
     
     lazy var closeStaticLabel: UILabel = {
@@ -96,14 +108,85 @@ class AuctionProductCell: UICollectionViewCell {
         
         addSubview(productImageView)
         addSubview(productDescriptionLabel)
-        addSubview(closeStaticLabel)
-        addSubview(closeTimeLabel)
+        addSubview(closeView)
+        closeView.addSubview(closeStaticLabel)
+        closeView.addSubview(closeTimeLabel)
         addSubview(priceStaticLabel)
         addSubview(priceLabel)
     }
 
     private func makeConstraints() {
-        // TODO: create constraints for this cell
+        
+        endView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+                .inset(appearance.endViewOffsets)
+            make.height.equalTo(appearance.endViewHeight)
+        }
+        
+        auctionImageView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+                .offset(appearance.auctionImageViewOffset)
+            make.size.equalTo(appearance.auctionImageViewSize)
+        }
+        
+        endDateLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+                .offset(appearance.xxsSpace)
+            make.leading.equalTo(auctionImageView.snp.trailing)
+                .offset(appearance.auctionImageViewOffset)
+            make.trailing.equalToSuperview()
+                .inset(appearance.xxxsSpace)
+        }
+        
+        productImageView.snp.makeConstraints { make in
+            make.top.equalTo(endView.snp.bottom)
+                .offset(appearance.endViewOffsets)
+            make.leading.trailing.equalToSuperview()
+                .inset(appearance.endViewOffsets)
+            make.width.equalTo(appearance.productImageViewWidth)
+            make.height.equalTo(appearance.productImageViewHeight)
+        }
+        
+        productDescriptionLabel.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+                .offset(appearance.endViewOffsets)
+        }
+        
+        closeView.snp.makeConstraints { make in
+            make.top.equalTo(productDescriptionLabel.snp.bottom)
+                .offset(appearance.endViewOffsets)
+            make.leading.trailing.equalToSuperview()
+                .inset(appearance.endViewOffsets)
+        }
+        
+        closeStaticLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        closeTimeLabel.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+            make.leading.greaterThanOrEqualTo(closeStaticLabel.snp.trailing)
+                .offset(appearance.endViewOffsets)
+            
+        }
+        
+        priceStaticLabel.snp.makeConstraints { make in
+            make.top.equalTo(closeView.snp.bottom)
+                .offset(appearance.endViewOffsets)
+            make.leading.equalToSuperview()
+                .offset(appearance.endViewOffsets)
+            make.bottom.equalToSuperview()
+                .inset(appearance.sSpace)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceStaticLabel.snp.top)
+            make.trailing.equalToSuperview()
+                .offset(appearance.endViewOffsets)
+            make.leading.greaterThanOrEqualTo(priceStaticLabel.snp.trailing)
+        }
+        
     }
 
 }
